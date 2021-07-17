4806afffd3de3c28de012cd6b1da6df46725646a
Eyitayo Olaigbe <tayoolaigbe@outlook.com>
2021-07-17 05:57:07 +0100

    Completed lift-off2

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -3,11 +3,11 @@ const resolvers = {
 		// returns an array of Tracks that will be used to populate
 		// the homepage grid of our web client
 		tracksForHome: (_, __, { dataSources }) => {
-			return dataSources.trackAPI.getTracksFromHome();
+			return dataSources.trackAPI.getTracksForHome();
 		},
 	},
 
-	Tracks: {
+	Track: {
 		// returns the author of a particular Track
 		author: ({ authorId }, _, { dataSources }) => {
 			return dataSources.trackAPI.getAuthor(authorId);
