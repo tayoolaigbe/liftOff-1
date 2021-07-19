3f134727502ac60ac5d7b3ecf7937f8f2c2878f5
Eyitayo Olaigbe <tayoolaigbe@outlook.com>
2021-07-19 16:12:52 +0100

    STarting to work on client side

---
+++ client/src/pages/track.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Layout } from '../components';
+import { gql, useQuery } from '@apollo/client';
+import QueryResult from '../components/query-result';
+
+const Track = ({ trackId }) => {
+	return <Layout></Layout>;
+};
+
+export default Track;
