7460ecf69604815ef444078d1159cfdad62b4534
Eyitayo Olaigbe <tayoolaigbe@outlook.com>
2021-07-20 06:53:11 +0100

    Working on the incrementTrackViews mutation

--- server/src/schema.js
+++ server/src/schema.js
@@ -8,6 +8,22 @@ const typeDefs = gql`
 		track(id: ID!): Track
 	}
 
+	type Mutation {
+		"Mutation to update the number of track views of a particular track"
+		incrementTrackViews(id: ID!): IncrementTrackViewsResponse!
+	}
+
+	type IncrementTrackViewsResponse {
+		"Similar to HTTP status code, represents the status of the mutation"
+		code: Int!
+		"Indicates whether the mutation was successful"
+		success: Boolean!
+		"Human-readable message for the UI"
+		message: String!
+		"Newly updated track after a successful mutation"
+		track: Track
+	}
+
 	"A track is a group of module that teaches about a specific topic."
 	type Track {
 		id: ID!
